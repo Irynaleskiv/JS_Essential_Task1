8b2819a2e0e21b7d8cb84cace8e95b60979a4089
Iryna Leskiv <irynaleskiv@online.ua>
2017-05-17 19:01:44 +0300

    Added index.html & script.js files

---
+++ script.js
@@ -0,0 +1,6 @@
+var name = "Alex";
+var time = "12 a.m.";
+var place = "Lviv Polytechnic National University";
+var event = prompt("What event do you have today?", "exam");
+console.log(name + "will have an" + event + "tomorrow at" + time + "in" + place + ".");
+alert(name + " has an " + event + " tomorrow at " + time + " in " + place + ".");
